41adf57f9a01bf608555e3ba01adb5ba6196a5af
James Quigley <jmquigley@outlook.com>
2017-01-27 14:40:44 -0600

    Adds unit test directory path builder to test.js

--- test.js
+++ test.js
@@ -1,7 +1,13 @@
 'use strict';
 
+const path = require('path');
 const test = require('ava');
+const timestamp = require('util.timestamp');
+const home = require('expand-home-dir');
+
+let unitTestDir = home(path.join('~/', '.tmp', `unit-test-data-${timestamp()}`));
 
 test('Empty, template test case', t => {
+	console.log(`Using test directory: ${unitTestDir}`);
 	t.pass();
 });

870e932f1c0b8552c8c2916d48b45a54538cfe7e
James Quigley <jmquigley@outlook.com>
2017-03-05 14:34:14 -0600

    Converted the rest of the project to typescript
    - Adds default helper script to testing
    - Upgraded sample tests.ts file to use mocha and removed ava

--- test/tests.ts
+++ test/tests.ts
@@ -1,19 +1,22 @@
 'use strict';
 
-import {test} from 'ava';
-import * as fs from 'fs-extra';
+import * as assert from 'assert';
 import {Fixture} from 'util.fixture';
+import {debug} from './helpers';
+import * as fs from 'fs-extra';
 
-test.after.always((t: any) => {
-	console.log('final cleanup: {module name}');
-	let directories = Fixture.cleanup();
-	directories.forEach((directory: string) => {
-		t.false(fs.existsSync(directory));
+describe('Executing test suite', () => {
+
+	after(() => {
+		debug('final cleanup: test_artifacts');
+		let directories = Fixture.cleanup();
+		directories.forEach((directory: string) => {
+			assert(!fs.existsSync(directory));
+		});
 	});
-});
 
-test('Empty, template test case', (t: any) => {
-	let fixture = new Fixture();
-	console.log(`Using test directory: ${fixture.dir}`);
-	t.pass();
+	it('Executing test', () => {
+		let fixture = new Fixture();
+		assert(fixture);
+	});
 });

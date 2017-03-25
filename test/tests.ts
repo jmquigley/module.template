bfe5877292582fd644026d6ea7c36be231fa7ccf
James Quigley <jmquigley@outlook.com>
2017-03-25 11:40:46 -0500

    Reverts test runner to ava

--- test/tests.ts
+++ test/tests.ts
@@ -1,22 +1,7 @@
 'use strict';
 
-import * as assert from 'assert';
-import * as fs from 'fs-extra';
-import {Fixture} from 'util.fixture';
-import {debug} from './helpers';
+import test from 'ava';
 
-describe('Executing test suite', () => {
-
-	after(() => {
-		debug('final cleanup: test_artifacts');
-		let directories = Fixture.cleanup();
-		directories.forEach((directory: string) => {
-			assert(!fs.existsSync(directory));
-		});
-	});
-
-	it('Executing test', () => {
-		let fixture = new Fixture();
-		assert(fixture);
-	});
+test('Base empty test case', t => {
+    t.pass();
 });
